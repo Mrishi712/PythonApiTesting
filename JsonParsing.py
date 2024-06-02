ac727b5e451489ade469a1c5d63c0b9f814b4ca0
Mrishi712 <itsmerishi712@gmail.com>
2024-06-02 14:27:35 +0530

    JsonParsing with loads with external files

--- JsonParsing.py
+++ JsonParsing.py
@@ -10,6 +10,18 @@ print(dict_myData["languages"])
 print(type(dict_myData["languages"]))
 languagesAvailable = dict_myData["languages"]
 firstLanguage = languagesAvailable[0]
+secondLanguage = dict_myData["languages"][1]
 print(firstLanguage)
+print(secondLanguage)
 for language in languagesAvailable:
     print(language)
+
+# parsing json from external file
+
+with open('SampleTestData/sampleJson.json', 'r') as reader:
+    currentContent = json.load(reader)
+    batters = currentContent["batters"]["batter"]
+    print(batters)
+    print(type(batters))
+    for idName in batters:
+        print(idName["type"])
